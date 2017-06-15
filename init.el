a2cefaa37fa7948c4dad384b01b63962e982b1fd
Taehan Kim <taehan75@gmail.com>
2017-06-16 08:51:18 +0900

    Set default font to Monaco

--- init.el
+++ init.el
@@ -1,16 +1,17 @@
 ;; disable unnecessaries
 (setq inhibit-splash-screen t)		;begin with *scratch*
-(toggle-scroll-bar -1)			;disable scrollbar
-(menu-bar-mode -1)			;disable menubar
-(tool-bar-mode -1)			;disable toolbar
+(toggle-scroll-bar -1)			    ;disable scrollbar
+(menu-bar-mode -1)			        ;disable menubar
+(tool-bar-mode -1)                  ;disable toolbar
 
 ;; package
 (require 'package)
 (setq package-archives '(
-			 ("melpa" . "https://melpa.org/packages/")
-			 ("org" . "http://orgmode.org/elpa/")
-			 ))
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ))
 (package-initialize)
 
-;; theme
+;; looking
 (load-theme 'zenburn t)
+(set-default-font "Monaco 13")
