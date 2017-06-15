60c89ebc28849163159796e8b38787f37c7dd243
Taehan Kim <taehan75@gmail.com>
2017-06-15 15:43:01 +0900

    menubar, toolbar, scrollbar disabled.

--- init.el
+++ init.el
@@ -1,1 +1,5 @@
+;; disable unnecessaries
 (setq inhibit-splash-screen t)		;begin with *scratch*
+(toggle-scroll-bar -1)			;disable scrollbar
+(menu-bar-mode -1)			;disable menubar
+(tool-bar-mode -1)			;disable toolbar
