53bf98933f43736625138a8f312e09165f1e510f
Taehan Kim <taehan75@gmail.com>
2017-06-16 15:30:04 +0900

    Added package exec-path-from-shell to inherit GOPATH from shell. It needed to execute goimports, godef, etc. from emacs.

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; disable unnecessaries
-(setq inhibit-splash-screen t)		;begin with *scratch*
-(toggle-scroll-bar -1)			    ;disable scrollbar
-(menu-bar-mode -1)			        ;disable menubar
+(setq inhibit-splash-screen t)      ;begin with *scratch*
+(toggle-scroll-bar -1)              ;disable scrollbar
+(menu-bar-mode -1)                  ;disable menubar
 (tool-bar-mode -1)                  ;disable toolbar
 
 ;; package
@@ -12,15 +12,28 @@
                          ))
 (package-initialize)
 
+;; shell environment
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH")
+  )
+
 ;; Go mode
 (add-hook 'go-mode-hook
           (lambda ()
+	    ;; compile
             (setq compile-command "go build -v && go test -v && go vet")
-            (define-key (current-local-map) "\C-c\C-c" 'compile)
+            (local-set-key (kbd "C-c C-c") 'compile)
+            (local-set-key (kbd "C-c C-r") 'recompile)
+            (local-set-key (kbd "M-]") 'next-error)
+            (local-set-key (kbd "M-[") 'previous-error)
+	    ;; source formatting
             (setq gofmt-command "goimports")                ;goimports for gofmt
             (add-hook 'before-save-hook 'gofmt-before-save) ;gofmt everytime
             (local-set-key (kbd "C-c m") 'gofmt)
-            (local-set-key (kbd "M-.") 'godef-jump)
+	    ;; source browsing
+            (local-set-key (kbd "M-.") 'godef-jump)         ;visit definition
+            (local-set-key (kbd "M-*") 'pop-tag-mark)       ;come back
             (auto-complete-mode 1)
             ))
 
@@ -36,7 +49,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flymake-go go-autocomplete auto-complete zenburn-theme go-mode))))
+    (exec-path-from-shell flymake-go go-autocomplete auto-complete zenburn-theme go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
