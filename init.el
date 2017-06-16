02080fde27e89fc5289bbac83c801148ee852df1
Taehan Kim <taehan75@gmail.com>
2017-06-16 09:14:06 +0900

    For Go flymake is set

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (go-autocomplete auto-complete zenburn-theme go-mode))))
+    (flymake-go go-autocomplete auto-complete zenburn-theme go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
