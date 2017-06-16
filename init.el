c64b52590f899ce6543300c17b5d648e990cd6a0
Taehan Kim <taehan75@gmail.com>
2017-06-16 09:10:55 +0900

    For Go autocomple is set

--- init.el
+++ init.el
@@ -12,6 +12,29 @@
                          ))
 (package-initialize)
 
+;; Go mode
+(add-hook 'go-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook 'gofmt-before-save) ;gofmt everytime
+            (auto-complete-mode 1)
+            ))
+
 ;; looking
 (load-theme 'zenburn t)
 (set-default-font "Monaco 13")
+
+;; from package-install
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-autocomplete auto-complete zenburn-theme go-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
